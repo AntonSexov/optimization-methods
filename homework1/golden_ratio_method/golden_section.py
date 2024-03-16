dd4dd02b57135904cb954730f25d0e29a8c320de
AntonSexov <antonvedernikov2003@gmail.com>
2024-03-16 20:30:14 +0300

    golden_secvtion method added

---
+++ homework1/golden_ratio_method/golden_section.py
@@ -0,0 +1,40 @@
+import math
+import numpy as np
+
+def golden_section_search(a, b, eps, f, out_file=None):
+    output = ""
+    iter_n = 1
+
+    phi = (1 + math.sqrt(5)) / 2
+    c = b - (b - a) / phi
+    d = a + (b - a) / phi
+
+    while np.abs(b - a) >= 2 * eps:
+        if iter_n > 25:
+            output += "Лимит итераций превышен \n"
+            break
+
+        if f(c) < f(d):
+            b = d
+            d = c
+            c = b - (b - a) / phi
+        else:
+            a = c
+            c = d
+            d = a + (b - a) / phi
+
+        output += f"Номер итерации: {iter_n} | a = {a} | b = {b} | c = {c} | d = {d} | f(c) = {f(c)} | f(d) = {f(d)} \n"
+        iter_n += 1
+
+    if out_file is not None:
+        min_value = min(f(c), f(d))
+        min_x = c if f(c) < f(d) else d
+        output += f"Ответ найден | Минимум функции {min_value} при x = {min_x} \n"
+        with open(out_file, "w") as file:
+            file.write(output)
+    else:
+        print(output)
+
+if __name__ == "__main__":
+    f = lambda x: np.log(1 + x**2) - np.sin(x)
+    golden_section_search(0, np.pi / 4, 10 ** -10, f, "golden_section_output.log")
